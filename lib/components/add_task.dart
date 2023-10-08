66393536e442e2a98e48836fecc647d79bd97ce3
Cfcool100 <cfcool100@gmail.com>
2023-10-08 21:39:53 +0000

    implementation of pushing task data form provider

--- lib/components/add_task.dart
+++ lib/components/add_task.dart
@@ -1,61 +1,71 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../constants.dart';
+import '../models/task.dart';
+import '../models/task_data.dart';
 
-class AddTasks extends StatelessWidget {
+class AddTasks extends StatefulWidget {
   const AddTasks({super.key});
 
   @override
+  State<AddTasks> createState() => _AddTasksState();
+}
+
+class _AddTasksState extends State<AddTasks> {
+  late String title;
+
+  @override
   Widget build(BuildContext context) {
+    final taskData = Provider.of<TaskData>(context);
     return Container(
-      padding: const EdgeInsets.symmetric(
-        vertical: 25.0,
-        horizontal: 30.0,
-      ),
-      width: double.infinity,
-      decoration: BoxDecoration(
-        borderRadius: const BorderRadius.only(
-          topLeft: Radius.circular(25),
-          topRight: Radius.circular(25),
+      color: const Color(0XFF757575),
+      child: Container(
+        padding: const EdgeInsets.symmetric(
+          vertical: 25.0,
+          horizontal: 30.0,
         ),
-        color: const Color(0xff6750a4).withOpacity(.08),
-      ),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          const Center(
-            child: Text(
+        decoration: kContainerCornerStyle,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            const Text(
               'Add Task',
+              textAlign: TextAlign.center,
               style: TextStyle(
-                color: Colors.lightBlueAccent,
+                color: kPrimaryColor,
                 fontSize: 30.0,
                 fontWeight: FontWeight.w500,
               ),
             ),
-          ),
-          TextField(
-            style: const TextStyle(color: Colors.black),
-            onChanged: (value) {
-              //
-            },
-          ),
-          const SizedBox(
-            height: 15.0,
-          ),
-          TextButton(
-            style: const ButtonStyle(
-              fixedSize: MaterialStatePropertyAll(Size(0, 50.0)),
-              backgroundColor: MaterialStatePropertyAll(Colors.lightBlueAccent),
+            TextField(
+              autofocus: true,
+              style: const TextStyle(color: Colors.black),
+              onChanged: (value) {
+                title = value;
+              },
             ),
-            onPressed: () {},
-            child: const Text(
-              'Add',
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 18.0,
+            const SizedBox(
+              height: 35.0,
+            ),
+            TextButton(
+              style: kElevatedButtonStyle,
+              onPressed: () {
+                setState(() {
+                  taskData.addTask(Task(title: title));
+                  Navigator.pop(context);
+                });
+              },
+              child: const Text(
+                'Add',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 18.0,
+                ),
               ),
-            ),
-          )
-        ],
+            )
+          ],
+        ),
       ),
     );
   }
