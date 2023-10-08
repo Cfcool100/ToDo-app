4eb6b1ef664f79be6f6b85301db751e941f918e9
Cfcool100 <cfcool100@gmail.com>
2023-10-08 21:38:00 +0000

    Listview builder implementation + fetching form provider

--- lib/components/task_list.dart
+++ lib/components/task_list.dart
@@ -1,22 +1,37 @@
 import 'package:flutter/material.dart';
-
+import 'package:provider/provider.dart';
+import 'package:todoey/models/task_data.dart';
 import 'task_tile.dart';
 
-class TasksList extends StatelessWidget {
+class TasksList extends StatefulWidget {
   const TasksList({super.key});
 
   @override
+  State<TasksList> createState() => _TasksListState();
+}
+
+class _TasksListState extends State<TasksList> {
+  @override
   Widget build(BuildContext context) {
-    return ListView(
+    final taskData = Provider.of<TaskData>(context);
+    return ListView.builder(
       padding: const EdgeInsets.symmetric(
         vertical: 30.0,
       ),
-      children: const [
-        TasksTile(),
-        TasksTile(),
-        TasksTile(),
-        TasksTile(),
-      ],
+      itemBuilder: (context, index) {
+        final task = taskData.tasks![index];
+        return TasksTile(
+          taskTitle: task.title!,
+          isChecked: task.isDone,
+          onLongPress: () => taskData.deleteTask(task),
+          callBack: (isChecked) {
+            setState(() {
+              task.toggleDone();
+            });
+          },
+        );
+      },
+      itemCount: taskData.tasks?.length,
     );
   }
 }
